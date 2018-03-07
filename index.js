ad4e272e0d5ea2c1202b9345ee71e98b386f8dd8
graberzz <mrneshta@gmail.com>
2018-03-08 03:51:22 +0500

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const axios = require('axios');
+const { JSDOM } = require("jsdom");
+
+const app = express();
+
+const isDubbed = (title) => {
+    const searchLink = 'https://www2.9anime.is/search?keyword=' + encodeURIComponent(title);
+    return axios(searchLink)
+        .then(({ data: html }) => new JSDOM(html))
+        .then(dom => {
+            const dubbedAnimeList = [...dom.window.document.querySelectorAll('a[data-jtitle]')]
+                              .map(el => ({title: el.dataset.jtitle.toLowerCase(),
+                                           link: el.href})
+                                        )
+                              .filter(({title}) => title.includes('dub'));
+            
+            if (dubbedAnimeList.length === 0) {
+                return {found: false, link: searchLink};
+            }
+            const targetAnime = dubbedAnimeList.find(anime => anime.title.includes(title.toLowerCase()));
+
+            return targetAnime !== undefined ? {...targetAnime, found: true} : false;
+        });
+
+};
+
+app.get('/', (req, res) => {
+    const title = req.query.title;
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    isDubbed(title)
+        .then(result => res.json(result));
+})
+
+app.listen(8080, () => {
+    console.log('Listening...');
+});
